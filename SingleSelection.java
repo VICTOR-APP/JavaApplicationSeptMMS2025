572b0a03b4194c02351e8664b832dc66873bce05
VICTOR <victor@VICTORs-MacBook-Pro.local>
2026-05-21 09:50:41 +0100

    first commit

---
+++ SingleSelection.java
@@ -0,0 +1,10 @@
+public class SingleSelection{
+         public static void main(String[] argus){
+		 int age = 21;
+		 
+		 if(age >= 18){
+		 System.out.printf("oh wow , you are  %d years old and you are nnow an adult%n",age);
+		 System.out.println("I am happy to be an adult");
+        }
+    }
+}
